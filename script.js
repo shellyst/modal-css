bc6c7df7ed85530e10e5f65e9f4c30ac7aab2509
Michelle Stone <michellechristine.s91@gmail.com>
2022-06-02 23:35:07 -0400

    Updated modal functionality to close when escape button is clicked.

--- script.js
+++ script.js
@@ -7,16 +7,29 @@ const btnCloseModal = document.querySelector('.close-modal');
 // To select something that has multiple.
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log(`Button clicked.`);
-    // Don't need the '.' before the class name since you're already specifying it's a class using classList.
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
+// Keep code dry by keeping copied code into a single function.
 
-btnCloseModal.addEventListener('click', function () {
+// Don't need the '.' before the class name since you're already specifying it's a class using classList.
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal());
+
+// Listening to events everywhere.
+// The event is an object that logs whatever key is pressed in the console - get information about the event itself including what key was pressed.
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
